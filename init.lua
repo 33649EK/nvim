74b76d2a1f7bd3835e6028ddda29f53df8662115
Evan Keller <33649ek@gmail.com>
2025-07-16 19:37:21 -0600

    attempted to set new color scheme

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.number = true
+vim.g.colors_name = "tokyonight"
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_transparent = true
